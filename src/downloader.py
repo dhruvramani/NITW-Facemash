3beb1f6ebb518179db5cf577b0208af54408a7f7
dhruvramani <dhruvramani98@gmail.com>
2016-12-23 13:16:53 +0530

    Migrated to Requests

--- src/downloader.py
+++ src/downloader.py
@@ -1,10 +1,14 @@
-from urllib.request import urlretrieve
+import requests
 
 def main():
     for i in range(100000, 1000000):
         try :
+            r = requests.get("https://wsdc.nitw.ac.in/student/assets/upload/thumbs/{}.jpg".format(i))
+            if 'html' in r.headers['content-type'] :
+                raise Exception('Roll Number Doesn\'t exist')
             print("=> Dowloading Roll Number {} :=> ".format(i), end = "")
-            urlretrieve("https://wsdc.nitw.ac.in/student/assets/upload/thumbs/{}.jpg".format(i), "{}.jpg".format(i))
+            with open("../images/{}.jpg".format(i), "wb") as img :
+                img.write(r.content)
             print("Done")
         except :
             print("=> Roll Number {} doesn't exist.".format(i))
